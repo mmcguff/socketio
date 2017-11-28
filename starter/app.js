a0309510fe3a04d7921e6a4d274733d3d7a32dcf
Matthew McGuff <matthewmcguff85@gmail.com>
2017-11-28 07:46:35 -0700

    Port changed to 8080 and added socket io connection pieces
    At this point in the process I have a socketio connection working.

--- starter/app.js
+++ starter/app.js
@@ -3,9 +3,14 @@ var path = require('path');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var port = 3000;
+var port = 8080;
 
 app.use(express.static(path.join(__dirname,"public")));
+
+io.on('connection',function (socket){
+    console.log('new connection made');
+});
+
 
 server.listen(port, function(){
     console.log('Listening on port ' + port);
